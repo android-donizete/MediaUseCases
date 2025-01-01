3306b8cbacf7aab6007b73689dc493bc1d2618a5
DonizeteVida <donizetevida1998@outlook.com>
2025-01-01 19:41:31 -0300

    CameraX runtime permission request

--- camerax/src/main/java/com/example/camerax/CameraXFeatureEntryPoint.kt
+++ camerax/src/main/java/com/example/camerax/CameraXFeatureEntryPoint.kt
@@ -1,18 +1,29 @@
 package com.example.camerax
 
 import android.content.Context
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.core.content.PermissionChecker
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -21,6 +32,10 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.enums.EnumEntries
+
+private val PERMISSIONS = arrayOf(
+    android.Manifest.permission.CAMERA
+)
 
 @Singleton
 class CameraXFeatureEntryPoint @Inject constructor(
@@ -58,15 +73,42 @@ fun CameraXFeaturesScreen(
     features: EnumEntries<CameraXFeature>,
     onClick: (CameraXFeature) -> Unit
 ) {
-    LazyColumn(
-        Modifier.fillMaxSize()
+    val context = LocalContext.current
+    var isPermissionGranted by remember { mutableStateOf(false) }
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
     ) {
-        items(features.toList()) { cameraXFeature ->
-            Card(
-                onClick = { onClick(cameraXFeature) }
-            ) {
-                Text(cameraXFeature.name)
+        isPermissionGranted = it.values.all { it }
+    }
+    LaunchedEffect(Unit) {
+        isPermissionGranted = PERMISSIONS.all { permission ->
+            PermissionChecker.checkSelfPermission(
+                context,
+                permission
+            ) == PermissionChecker.PERMISSION_GRANTED
+        }
+    }
+    if (isPermissionGranted) {
+        LazyColumn(
+            Modifier.fillMaxSize()
+        ) {
+            items(features.toList()) { cameraXFeature ->
+                Card(
+                    onClick = { onClick(cameraXFeature) }
+                ) {
+                    Text(cameraXFeature.name)
+                }
             }
+        }
+        return
+    }
+
+    Box(Modifier.fillMaxSize()) {
+        Button(
+            onClick = { launcher.launch(PERMISSIONS) },
+            Modifier.align(Alignment.Center)
+        ) {
+            Text("We need some permissions")
         }
     }
 }
